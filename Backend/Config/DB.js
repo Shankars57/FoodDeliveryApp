0c8a0df1a07e19727e6349f2087943e25167916c
MadCoder7 <shankarpubg4@gmail.com>
2025-04-16 19:55:15 +0530

    hi

--- Backend/Config/DB.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://shankar:shankar@cluster0.veb6r.mongodb.net/food-del"
-    )
-    .then(() => console.log("DB connected"));
-};
